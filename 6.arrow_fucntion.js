da618f43e3045058f939428f94654a60cb27001c
themdshakibul <mdshakibul88240@gmail.com>
2026-03-01 22:49:35 +0600

    arrow fucntion

---
+++ 6.arrow_fucntion.js
@@ -0,0 +1,9 @@
+// Traditional function
+console.log("Multiplication -->", multiply(10, 5));
+
+function multiply(a, b) {
+  return a * b;
+}
+
+// const output = multiply(10, 5);
+// console.log(output); // 50
